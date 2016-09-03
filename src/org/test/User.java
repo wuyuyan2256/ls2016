a0ba7514f3796555d2651baca8246e70a4f6a328
wuyuyan2256 <wuyuyan2256@163.com>
2016-09-03 13:50:05 +0800

    Print "ls2016"

---
+++ src/org/test/User.java
@@ -0,0 +1,25 @@
+package org.test;
+
+public class User {
+
+	private String UserId;
+	
+	private String UserName;
+	
+	public String getUserId() {
+		return UserId;
+	}
+
+	public void setUserId(String userId) {
+		UserId = userId;
+	}
+
+	public String getUserName() {
+		return UserName;
+	}
+
+	public void setUserName(String userName) {
+		UserName = userName;
+	}
+
+}
